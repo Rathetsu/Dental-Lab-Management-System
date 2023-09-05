4e3270f7c4cd78292d8b73ac3da9ff9683e33f88
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2023-09-05 23:18:26 +0300

    test

--- DentalLabManagementSystem/metro.config.js
+++ DentalLabManagementSystem/metro.config.js
@@ -1,7 +1,7 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
- * Metro configuration
+ * Metro configuration zz
  * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
